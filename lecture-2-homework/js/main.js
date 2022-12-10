7e7cc391c8a74db8e4e977b9c15675fa16dda88e
Boris Shagiev <boris260692@gmail.com>
2022-12-10 16:14:44 +0200

    add finished homework

---
+++ lecture-2-homework/js/main.js
@@ -0,0 +1,29 @@
+let startNumber = checkNumber(Number(prompt('Введіть початкове значення')))
+let lastNumber = checkNumber(Number(prompt('Введіть кінцеве значення')))
+
+// Перевірка на цілісність числа
+function checkNumber(number){
+    if (Number.isInteger(number)) {
+        return number = number
+    } else if (!Number.isInteger(number)) {
+        integerNumber = Number(prompt('Введіть ціле число'))
+        if (!Number.isInteger(integerNumber)){
+            const confirmQuestion = confirm('Введене значення знову не являється цілим, бажаєте здійснити округлення?')
+            if (confirmQuestion){
+                return number = Math.round(integerNumber)
+            }
+        }
+    }
+}
+const confirmRequest = confirm("Бажаєте пропускати парні числа ?")
+let sumNum = 0
+for (let i = startNumber; i <= lastNumber; i++) {
+    if (confirmRequest){
+        if (i % 2){
+            sumNum = sumNum + i
+        }
+    } else {
+        sumNum = sumNum + i
+    }
+}
+console.log(sumNum);
