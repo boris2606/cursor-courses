a126b56fe9142cd3b81f57093576c95d9904b9ed
Boris Shagiev <boris260692@gmail.com>
2022-12-14 21:15:52 +0200

    Add folder homework 3

---
+++ lecture-3-homework/js/main.js
@@ -0,0 +1,29 @@
+let startNumber = checkNumber(Number(prompt('Введіть початкове значення')))
+let lastNumber = checkNumber(Number(prompt('Введіть кінцеве значення')))
+
+// Перевірка на цілісність числа
+function checkNumber(number){
+    if (Number.isInteger(number)) {
+        return number = number
+    } else if (!Number.isInteger(number)) {
+        integerNumber = Number(prompt('Введіть ціле число'))
+        if (!Number.isInteger(integerNumber)){
+            const confirmQuestion = confirm('Введене значення знову не являється цілим, бажаєте здійснити округлення?')
+            if (confirmQuestion){
+                return number = Math.round(integerNumber)
+            }
+        }
+    }
+}
+const confirmRequest = confirm("Бажаєте пропускати парні числа ?")
+let sumNum = 0
+for (let i = startNumber; i <= lastNumber; i++) {
+    if (confirmRequest){
+        if (i % 2){
+            sumNum = sumNum + i
+        }
+    } else {
+        sumNum = sumNum + i
+    }
+}
+document.querySelector('.result_sum').innerHTML = `Сума чисал становить <span class="colored">${sumNum}</span> при обранні початкового значення: <span class="colored">${startNumber}</span> кінцевого значення: <span class="colored">${lastNumber}</span> (Статус пропуску парних чисел <span class="colored">"${confirmRequest}</span>")`
