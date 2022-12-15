a4cf4cc01215ae90856b61a4ae2fc8db3036fa84
Boris Shagiev <boris260692@gmail.com>
2022-12-15 02:26:58 +0200

    Complete 5 functions

--- lecture-3-homework/js/main.js
+++ lecture-3-homework/js/main.js
@@ -1,29 +1,51 @@
-let startNumber = checkNumber(Number(prompt('Введіть початкове значення')))
-let lastNumber = checkNumber(Number(prompt('Введіть кінцеве значення')))
+// Функція №1 Створити функцію getMaxDigit(number) – яка отримує будь-яке число та виводить найбільшу цифру в цьому числі.
 
-// Перевірка на цілісність числа
-function checkNumber(number){
-    if (Number.isInteger(number)) {
-        return number = number
-    } else if (!Number.isInteger(number)) {
-        integerNumber = Number(prompt('Введіть ціле число'))
-        if (!Number.isInteger(integerNumber)){
-            const confirmQuestion = confirm('Введене значення знову не являється цілим, бажаєте здійснити округлення?')
-            if (confirmQuestion){
-                return number = Math.round(integerNumber)
-            }
-        }
+const numberFuncOne = prompt('Введіть будь-яке число щоб отримати найбильшу цифру в цьому числі')
+function getMaxDigit(number){
+    if (number){
+        let filteredArr = number.split('')
+                                .filter(errElem => errElem !== '.' && errElem !== ',')
+                                .map(Number)
+                                .filter(Boolean)
+        return Math.max(...filteredArr);
     }
 }
-const confirmRequest = confirm("Бажаєте пропускати парні числа ?")
-let sumNum = 0
-for (let i = startNumber; i <= lastNumber; i++) {
-    if (confirmRequest){
-        if (i % 2){
-            sumNum = sumNum + i
-        }
-    } else {
-        sumNum = sumNum + i
+console.log(`Функція №1: ${getMaxDigit(numberFuncOne)}`); 
+
+//Функція №2 Створити функцію, яка визначає ступінь числа. Не використовуючи Math.pow та **. Використовуйте цикл
+const number = prompt('Введіть число яке необхідно підняти до степеня')
+const expon = prompt('Введіть степінь')
+function nuberExpon(number, exp){
+    let result = 1
+    for (let i = 0; i < exp; i++) {
+        result *= number
     }
+    return result
 }
-document.querySelector('.result_sum').innerHTML = `Сума чисал становить <span class="colored">${sumNum}</span> при обранні початкового значення: <span class="colored">${startNumber}</span> кінцевого значення: <span class="colored">${lastNumber}</span> (Статус пропуску парних чисел <span class="colored">"${confirmRequest}</span>")`
+console.log(`Функція №2: ${nuberExpon(number,expon)}`); 
+
+//Функція №3 Створити функцію, яка форматує ім'я, роблячи першу букву великою.
+const personName = prompt("Введіть своє ім'я")
+function fixName(name){
+    const fixRegister = name.toLowerCase()
+    return fixRegister[0].toUpperCase() + fixRegister.slice(1)
+}
+console.log(`Функція №3: ${fixName(personName)}`); 
+
+// Функція №4 Створити функцію, яка вираховує суму, що залишається після оплати податку від зарабітньої плати.
+const cashWithOutTax = prompt('Введіть Вашу заробітню плату')
+function cleanCash(cash){
+    let tax = 18 + 1.5
+    return cash - ((cash / 100) * tax)
+}
+console.log(`Функція №4: ${cleanCash(cashWithOutTax)}`); 
+
+// Функція №5 Створити функцію, яка повертає випадкове ціле число в діапазоні від N до M.
+let minValue = prompt('Введіть мінімальне значення для діапазону')
+let maxValue = prompt('Введіть максимальне значення діапазону')
+function getRandomNumber(min,max){
+    return Math.round(Math.random() * (max - min +1) + min)
+}
+console.log(`Функція №5: ${getRandomNumber(minValue,maxValue)}`);
+
+// Функція №6 Створити функцію, яка рахує скільки разів певна буква повторюється в слові.
