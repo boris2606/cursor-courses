56dd485fc9db5ac4c577df36c5ea9b0db1c82a40
Boris Shagiev <boris260692@gmail.com>
2023-01-23 16:19:11 +0200

    Fix JS

--- lecture-12-homework/js/actors.js
+++ lecture-12-homework/js/actors.js
@@ -3,246 +3,246 @@ export const actors = {
         "url": "../lecture-12-homework/assets/img/actors/C-3PO.jpeg"
     },
     "R2-D2": {
-        "url": "../assets/img/actors/R2-D2.webp"
+        "url": "../lecture-12-homework/assets/img/actors/R2-D2.webp"
     },
     "Darth Vader": {
-        "url": "../assets/img/actors/Darthvader.jpeg"
+        "url": "../lecture-12-homework/assets/img/actors/Darthvader.jpeg"
     },
     "Greedo": {
-        "url": "../assets/img/actors/Greedo.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Greedo.webp"
     },
     "Leia Organa": {
-        "url": "../assets/img/actors/Leiaorgana.jpeg"
+        "url": "../lecture-12-homework/assets/img/actors/Leiaorgana.jpeg"
     },
     "Luke Skywalker": {
-        "url": "../assets/img/actors/Lukeskywalker.jpeg"
+        "url": "../lecture-12-homework/assets/img/actors/Lukeskywalker.jpeg"
     },
     "R5-D4": {
-        "url": "../assets/img/actors/R5-D4.webp"
+        "url": "../lecture-12-homework/assets/img/actors/R5-D4.webp"
     },
     "Owen Lars": {
-        "url": "../assets/img/actors/Owenlars.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Owenlars.webp"
     },
     "Beru Whitesun lars": {
-        "url": "../assets/img/actors/Beruwhitesunlars.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Beruwhitesunlars.webp"
     },
     "Han Solo": {
-        "url": "../assets/img/actors/Hansolo.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Hansolo.webp"
     },
     "Biggs Darklighter": {
-        "url": "../assets/img/actors/Biggsdarklighter.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Biggsdarklighter.webp"
     },
     "Wilhuff Tarkin": {
-        "url": "../assets/img/actors/Wilhufftarkin.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Wilhufftarkin.webp"
     },
     "Obi-Wan Kenobi": {
-        "url": "../assets/img/actors/Obiwankenobi.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Obiwankenobi.webp"
     },
     "Chewbacca": {
-        "url": "../assets/img/actors/Chewbacca.jpeg"
+        "url": "../lecture-12-homework/assets/img/actors/Chewbacca.jpeg"
     },
     "Wedge Antilles": {
-        "url": "../assets/img/actors/Wedgeantilles.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Wedgeantilles.webp"
     },
     "Jek Tono Porkins": {
-        "url": "../assets/img/actors/Jektonoporkins.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Jektonoporkins.webp"
     },
     "Raymus Antilles": {
-        "url": "../assets/img/actors/Raymusantilles.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Raymusantilles.webp"
     },
     "Jabba Desilijic Tiure": {
-        "url": "../assets/img/actors/Jabbadesilijictiure.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Jabbadesilijictiure.webp"
     },
     "Palpatine": {
-        "url": "../assets/img/actors/Palpatine.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Palpatine.webp"
     },
     "Yoda": {
-        "url": "../assets/img/actors/Yoda.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Yoda.webp"
     },
     "Lobot": {
-        "url": "../assets/img/actors/Lobot.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Lobot.webp"
     },
     "Bossk": {
-        "url": "../assets/img/actors/Bossk.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Bossk.webp"
     },
     "Boba Fett": {
-        "url": "../assets/img/actors/Bobafett.jpeg"
+        "url": "../lecture-12-homework/assets/img/actors/Bobafett.jpeg"
     },
     "Lando Calrissian": {
-        "url": "../assets/img/actors/Landocalrissian.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Landocalrissian.webp"
     },
     "IG-88": {
-        "url": "../assets/img/actors/IG-88.webp"
+        "url": "../lecture-12-homework/assets/img/actors/IG-88.webp"
     },
     "Ackbar": {
-        "url": "../assets/img/actors/Ackbar.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Ackbar.webp"
     },
     "Wicket Systri Warrick": {
-        "url": "../assets/img/actors/Wicketsystriwarrick.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Wicketsystriwarrick.webp"
     },
     "Mon Mothma": {
-        "url": "../assets/img/actors/Monmothma.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Monmothma.webp"
     },
     "Bib Fortuna": {
-        "url": "../assets/img/actors/Bibfortuna.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Bibfortuna.webp"
     },
     "Nien Nunb": {
-        "url": "../assets/img/actors/Niennunb.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Niennunb.webp"
     },
     "Arvel Crynyd": {
-        "url": "../assets/img/actors/Arvelcrynyd.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Arvelcrynyd.webp"
     },
     "Rugor Nass": {
-        "url": "../assets/img/actors/Rugornass.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Rugornass.webp"
     },
     "Sebulba": {
-        "url": "../assets/img/actors/Sebulba.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Sebulba.webp"
     },
     "Finis Valorum": {
-        "url": "../assets/img/actors/Finisvalorum.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Finisvalorum.webp"
     },
     "Saesee Tiin": {
-        "url": "../assets/img/actors/Saeseetiin.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Saeseetiin.webp"
     },
     "Nute Gunray": {
-        "url": "../assets/img/actors/Nutegunray.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Nutegunray.webp"
     },
     "Dud Bolt": {
-        "url": "../assets/img/actors/Dudbolt.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Dudbolt.webp"
     },
     "Qui-Gon Jinn": {
-        "url": "../assets/img/actors/Quigonjinn.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Quigonjinn.webp"
     },
     "Roos Tarpals": {
-        "url": "../assets/img/actors/Roostarpals.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Roostarpals.webp"
     },
     "Darth Maul": {
-        "url": "../assets/img/actors/Darthmaul.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Darthmaul.webp"
     },
     "Mas Amedda": {
-        "url": "../assets/img/actors/Masamedda.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Masamedda.webp"
     },
     "Jar Jar Binks": {
-        "url": "../assets/img/actors/Jarjarbinks.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Jarjarbinks.webp"
     },
     "Ratts Tyerel": {
-        "url": "../assets/img/actors/Rattstyerel.jpeg"
+        "url": "../lecture-12-homework/assets/img/actors/Rattstyerel.jpeg"
     },
     "Watto": {
-        "url": "../assets/img/actors/Watto.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Watto.webp"
     },
     "Gasgano": {
-        "url": "../assets/img/actors/Gasgano.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Gasgano.webp"
     },
     "Ben Quadinaros": {
-        "url": "../assets/img/actors/Benquadinaros.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Benquadinaros.webp"
     },
     "Padmé Amidala": {
-        "url": "../assets/img/actors/Padméamidala.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Padméamidala.webp"
     },
     "Anakin Skywalker": {
-        "url": "../assets/img/actors/Anakinskywalker.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Anakinskywalker.webp"
     },
     "Mace Windu": {
-        "url": "../assets/img/actors/Macewindu.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Macewindu.webp"
     },
     "Ric Olié": {
-        "url": "../assets/img/actors/Ricolié.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Ricolié.webp"
     },
     "Ayla Secura": {
-        "url": "../assets/img/actors/Aylasecura.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Aylasecura.webp"
     },
     "Quarsh Panaka": {
-        "url": "../assets/img/actors/Quarshpanaka.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Quarshpanaka.webp"
     },
     "Yarael Poof": {
-        "url": "../assets/img/actors/Yaraelpoof.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Yaraelpoof.webp"
     },
     "Plo Koon": {
-        "url": "../assets/img/actors/Plokoon.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Plokoon.webp"
     },
     "Shmi Skywalker": {
-        "url": "../assets/img/actors/Shmiskywalker.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Shmiskywalker.webp"
     },
     "Adi Gallia": {
-        "url": "../assets/img/actors/Adigallia.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Adigallia.webp"
     },
     "Kit Fisto": {
-        "url": "../assets/img/actors/Kitfisto.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Kitfisto.webp"
     },
     "Eeth Koth": {
-        "url": "../assets/img/actors/Eethkoth.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Eethkoth.webp"
     },
     "Ki-Adi-Mundi": {
-        "url": "../assets/img/actors/Kiadimundi.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Kiadimundi.webp"
     },
     "Luminara Unduli": {
-        "url": "../assets/img/actors/Luminaraunduli.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Luminaraunduli.webp"
     },
     "Sly Moore": {
-        "url": "../assets/img/actors/Slymoore.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Slymoore.webp"
     },
     "San Hill": {
-        "url": "../assets/img/actors/Sanhill.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Sanhill.webp"
     },
     "Dooku": {
-        "url": "../assets/img/actors/Dooku.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Dooku.webp"
     },
     "R4-P17": {
-        "url": "../assets/img/actors/R4-P17.webp"
+        "url": "../lecture-12-homework/assets/img/actors/R4-P17.webp"
     },
     "Gregar Typho": {
-        "url": "../assets/img/actors/Gregartypho.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Gregartypho.webp"
     },
     "Dormé": {
-        "url": "../assets/img/actors/Dormé.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Dormé.webp"
     },
     "Wat Tambor": {
-        "url": "../assets/img/actors/Wattambor.jpeg"
+        "url": "../lecture-12-homework/assets/img/actors/Wattambor.jpeg"
     },
     "Zam Wesell": {
-        "url": "../assets/img/actors/Zamwesell.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Zamwesell.webp"
     },
     "Jango Fett": {
-        "url": "../assets/img/actors/Jangofett.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Jangofett.webp"
     },
     "Dexter Jettster": {
-        "url": "../assets/img/actors/Dexterjettster.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Dexterjettster.webp"
     },
     "Shaak Ti": {
-        "url": "../assets/img/actors/Shaakti.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Shaakti.webp"
     },
     "Jocasta Nu": {
-        "url": "../assets/img/actors/Jocastanu.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Jocastanu.webp"
     },
     "Poggle the Lesser": {
-        "url": "../assets/img/actors/Pogglethelesser.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Pogglethelesser.webp"
     },
     "Taun We": {
-        "url": "../assets/img/actors/Taunwe.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Taunwe.webp"
     },
     "Bail Prestor Organa": {
-        "url": "../assets/img/actors/Bailprestororgana.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Bailprestororgana.webp"
     },
     "Barriss Offee": {
-        "url": "../assets/img/actors/Barrissoffee.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Barrissoffee.webp"
     },
     "Cordé": {
-        "url": "../assets/img/actors/Cordé.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Cordé.webp"
     },
     "Lama Su": {
-        "url": "../assets/img/actors/Lamasu.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Lamasu.webp"
     },
     "Cliegg Lars": {
-        "url": "../assets/img/actors/Cliegglars.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Cliegglars.webp"
     },
     "Tarfful": {
-        "url": "../assets/img/actors/Tarfful.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Tarfful.webp"
     },
     "Grievous": {
-        "url": "../assets/img/actors/Grievous.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Grievous.webp"
     },
     "Tion Medon": {
-        "url": "../assets/img/actors/Tionmedon.webp"
+        "url": "../lecture-12-homework/assets/img/actors/Tionmedon.webp"
     }
 }
--- lecture-12-homework/js/planets.js
+++ lecture-12-homework/js/planets.js
@@ -1,182 +1,182 @@
 export const planets = {
     "Polis Massa": {
-        "url": "../assets/img/planets/polis.webp"
+        "url": "../lecture-12-homework/assets/img/planets/polis.webp"
     },
     "Tatooine": {
-        "url": "../assets/img/planets/tatooine.jpeg"
+        "url": "../lecture-12-homework/assets/img/planets/tatooine.jpeg"
     },
     "Alderaan": {
-        "url": "../assets/img/planets/alderaan.webp"
+        "url": "../lecture-12-homework/assets/img/planets/alderaan.webp"
     },
     "Yavin IV": {
-        "url": "../assets/img/planets/yavin4.webp"
+        "url": "../lecture-12-homework/assets/img/planets/yavin4.webp"
     },
     "Hoth": {
-        "url": "../assets/img/planets/hoth.webp"
+        "url": "../lecture-12-homework/assets/img/planets/hoth.webp"
     },
     "Dagobah": {
-        "url": "../assets/img/planets/dagobah.webp"
+        "url": "../lecture-12-homework/assets/img/planets/dagobah.webp"
     },
     "Bespin": {
-        "url": "../assets/img/planets/bespin.webp"
+        "url": "../lecture-12-homework/assets/img/planets/bespin.webp"
     },
     "Endor": {
-        "url": "../assets/img/planets/endor.webp"
+        "url": "../lecture-12-homework/assets/img/planets/endor.webp"
     },
     "Naboo": {
-        "url": "../assets/img/planets/naboo.webp"
+        "url": "../lecture-12-homework/assets/img/planets/naboo.webp"
     },
     "Coruscant": {
-        "url": "../assets/img/planets/coruscant.webp"
+        "url": "../lecture-12-homework/assets/img/planets/coruscant.webp"
     },
     "Kamino": {
-        "url": "../assets/img/planets/kamino.webp"
+        "url": "../lecture-12-homework/assets/img/planets/kamino.webp"
     },
     "Geonosis": {
-        "url": "../assets/img/planets/geonosis.webp"
+        "url": "../lecture-12-homework/assets/img/planets/geonosis.webp"
     },
     "Utapau": {
-        "url": "../assets/img/planets/utapau.webp"
+        "url": "../lecture-12-homework/assets/img/planets/utapau.webp"
     },
     "Mustafar": {
-        "url": "../assets/img/planets/mustafar.webp"
+        "url": "../lecture-12-homework/assets/img/planets/mustafar.webp"
     },
     "Kashyyyk": {
-        "url": "../assets/img/planets/kashyyyk.webp"
+        "url": "../lecture-12-homework/assets/img/planets/kashyyyk.webp"
     },
     "Mygeeto": {
-        "url": "../assets/img/planets/mygeeto.webp"
+        "url": "../lecture-12-homework/assets/img/planets/mygeeto.webp"
     },
     "Felucia": {
-        "url": "../assets/img/planets/Felucia.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Felucia.webp"
     },
     "Cato Neimoidia": {
-        "url": "../assets/img/planets/Catoneimoidia.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Catoneimoidia.webp"
     },
     "Saleucami": {
-        "url": "../assets/img/planets/Saleucami.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Saleucami.webp"
     },
     "Stewjon": {
-        "url": "../assets/img/planets/Stewjon.jpeg"
+        "url": "../lecture-12-homework/assets/img/planets/Stewjon.jpeg"
     },
     "Eriadu": {
-        "url": "../assets/img/planets/Eriadu.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Eriadu.webp"
     },
     "Corellia": {
-        "url": "../assets/img/planets/Corellia.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Corellia.webp"
     },
     "Rodia": {
-        "url": "../assets/img/planets/Rodia.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Rodia.webp"
     },
     "Nal Hutta": {
-        "url": "../assets/img/planets/Nalhutta.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Nalhutta.webp"
     },
     "Dantooine": {
-        "url": "../assets/img/planets/Dantooine.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Dantooine.webp"
     },
     "Bestine IV": {
-        "url": "../assets/img/planets/Bestine.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Bestine.webp"
     },
     "Ord Mantell": {
-        "url": "../assets/img/planets/Ordmantell.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Ordmantell.webp"
     },
     "unknown": {
-        "url": "../assets/img/planet.png"
+        "url": "../lecture-12-homework/assets/img/planet.png"
     },
     "Trandosha": {
-        "url": "../assets/img/planets/Trandosha.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Trandosha.webp"
     },
     "Socorro": {
-        "url": "../assets/img/planets/Socorro.jpeg"
+        "url": "../lecture-12-homework/assets/img/planets/Socorro.jpeg"
     },
     "Mon Cala": {
-        "url": "../assets/img/planets/Moncala.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Moncala.webp"
     },
     "Chandrila": {
-        "url": "../assets/img/planets/Chandrila.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Chandrila.webp"
     },
     "Sullust": {
-        "url": "../assets/img/planets/Sullust.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Sullust.webp"
     },
     "Toydaria": {
-        "url": "../assets/img/planets/Toydaria.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Toydaria.webp"
     },
     "Malastare": {
-        "url": "../assets/img/planets/Malastare.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Malastare.webp"
     },
     "Dathomir": {
-        "url": "../assets/img/planets/Dathomir.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Dathomir.webp"
     },
     "Ryloth": {
-        "url": "../assets/img/planets/Ryloth.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Ryloth.webp"
     },
     "Aleen Minor": {
-        "url": "../assets/img/planets/Aleenminor.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Aleenminor.webp"
     },
     "Vulpter": {
-        "url": "../assets/img/planets/Vulpter.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Vulpter.webp"
     },
     "Troiken": {
-        "url": "../assets/img/planets/Troiken.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Troiken.webp"
     },
     "Tund": {
-        "url": "../assets/img/planets/Tund.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Tund.webp"
     },
     "Haruun Kal": {
-        "url": "../assets/img/planets/Haruunkal.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Haruunkal.webp"
     },
     "Cerea": {
-        "url": "../assets/img/planets/Cerea.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Cerea.webp"
     },
     "Glee Anselm": {
-        "url": "../assets/img/planets/Gleeanselm.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Gleeanselm.webp"
     },
     "Iridonia": {
-        "url": "../assets/img/planets/Iridonia.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Iridonia.webp"
     },
     "Tholoth": {
-        "url": "../assets/img/planets/Tholoth.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Tholoth.webp"
     },
     "Iktotch": {
-        "url": "../assets/img/planets/Iktotch.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Iktotch.webp"
     },
     "Quermia": {
-        "url": "../assets/img/planets/Quermia.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Quermia.webp"
     },
     "Dorin": {
-        "url": "../assets/img/planets/Dorin.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Dorin.webp"
     },
     "Champala": {
-        "url": "../assets/img/planets/Champala.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Champala.webp"
     },
     "Mirial": {
-        "url": "../assets/img/planets/Mirial.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Mirial.webp"
     },
     "Serenno": {
-        "url": "../assets/img/planets/Serenno.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Serenno.webp"
     },
     "Concord Dawn": {
-        "url": "../assets/img/planets/Concorddawn.jpeg"
+        "url": "../lecture-12-homework/assets/img/planets/Concorddawn.jpeg"
     },
     "Zolan": {
-        "url": "../assets/img/planets/Zolan.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Zolan.webp"
     },
     "Ojom": {
-        "url": "../assets/img/planets/Ojom.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Ojom.webp"
     },
     "Skako": {
-        "url": "../assets/img/planets/Skako.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Skako.webp"
     },
     "Muunilinst": {
-        "url": "../assets/img/planets/Muunilinst.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Muunilinst.webp"
     },
     "Shili": {
-        "url": "../assets/img/planets/Shili.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Shili.webp"
     },
     "Kalee": {
-        "url": "../assets/img/planets/Kalee.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Kalee.webp"
     },
     "Umbara": {
-        "url": "../assets/img/planets/Umbara.webp"
+        "url": "../lecture-12-homework/assets/img/planets/Umbara.webp"
     }
 }
